7164c539285016f53026714d3812753fc82b16da
Aleksandra Krasteva <I476232@fhict.nl>
2022-10-23 10:23:43 +0200

    Update database connection

--- src/main/java/sem3chatbot/backend/business/ServiceImpl/ResponseServiceImpl.java
+++ src/main/java/sem3chatbot/backend/business/ServiceImpl/ResponseServiceImpl.java
@@ -6,18 +6,24 @@ import sem3chatbot.backend.business.ResponseService;
 import sem3chatbot.backend.business.exception.NoBlankQuestionsException;
 import sem3chatbot.backend.domain.BotResponse;
 import sem3chatbot.backend.domain.UserInput;
+import sem3chatbot.backend.persistence.KeywordRepository;
+import sem3chatbot.backend.persistence.entity.KeywordEntity;
+
+import java.util.List;
 
 
 @Service
 @AllArgsConstructor
 public class ResponseServiceImpl implements ResponseService {
-
+KeywordRepository keywordRepository;
     public BotResponse processQuestion(UserInput request){
         if(request.getQuestion() == null || request.getQuestion().equals("")){
             throw new NoBlankQuestionsException();
         }
+        //check if db con works
+        List<KeywordEntity> keywordEntities = keywordRepository.findAll();
         return BotResponse.builder()
-                .response("we chillin")
+                .response(keywordEntities.get(0).getId().toString() + keywordEntities.get(0).getText())
                 .build();
     }
 }
