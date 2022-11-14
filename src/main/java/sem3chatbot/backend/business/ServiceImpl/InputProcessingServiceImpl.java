db0bba6f607f2b8a4241facc146a9ac79bc9996c
Tsvetislav Rangelov <cveti2002mnt@gmail.com>
2022-11-14 18:54:29 +0100

    removed question keywords

--- src/main/java/sem3chatbot/backend/business/ServiceImpl/InputProcessingServiceImpl.java
+++ src/main/java/sem3chatbot/backend/business/ServiceImpl/InputProcessingServiceImpl.java
@@ -21,7 +21,6 @@ public class InputProcessingServiceImpl implements InputProcessingService {
 
     private KeywordRepository keywordRepository;
     private AnswerRepository answerRepository;
-    private SolutionRepository solutionRepository;
 
     public HashMap<String, List<Long>> findMatches(UserInput request){
         //Long -> Long[]
@@ -33,11 +32,9 @@ public class InputProcessingServiceImpl implements InputProcessingService {
         //add new words to db when a user types a question
         List<Keyword> keywords = keywordEntities.stream().map(KeywordConverter::convert).toList();
         keywordEntities.clear();
-        List<Long> questionIds = groupKeywords(keywords, "question", question);
         List<Long> topicIds = groupKeywords(keywords, "topic", question);
         List<Long> auxiliaryIds = groupKeywords(keywords, "auxiliary", question);
 
-        matches.put("question", questionIds);
         matches.put("topic", topicIds);
         matches.put("auxiliary", auxiliaryIds);
 
@@ -50,10 +47,6 @@ public class InputProcessingServiceImpl implements InputProcessingService {
     public String findAnswer(HashMap<String, List<Long>> matchedKeywords) {
         // The response of the chatbot
         String resultAnswer = "No answer found. Try to rewrite your question.";
-
-        if(matchedKeywords.get("question").size() == 0 || matchedKeywords.get("topic").size() == 0)
-            return resultAnswer;
-
         // The higher the weight of a potential answer, the better it should be as a response for the given question.
         double currentWeight = 0;
 
@@ -92,10 +85,9 @@ public class InputProcessingServiceImpl implements InputProcessingService {
         List<Long> convertedIdList = ConvertFromStringToList(keywordsById);
 
         // The amount of keywords that matched by the given type
-        double amountMatchedByType = convertedIdList.stream().filter(id ->matchedKeywordsIdByType.contains(id)).collect(Collectors.toList()).size();
+        double amountMatchedByType = convertedIdList.stream().filter(matchedKeywordsIdByType::contains).collect(Collectors.toList()).size();
 
-        if(amountMatchedByType == 0)
-            return 0;
+        if(amountMatchedByType == 0) return 0;
 
         // The percentage of: amount of matched keywords/total keywords in answer of this type
         double pctMatchedInDB = amountMatchedByType/convertedIdList.size();
@@ -107,20 +99,13 @@ public class InputProcessingServiceImpl implements InputProcessingService {
     }
 
     private double CalculateTotalWeight(HashMap<String, List<Long>> matchedKeywords, Answer answer){
-        double resultedWeight = 0;
+        double resultedWeight;
 
-        // Question keyword has to contain at least one match, else the answer is not suitable.
-        // This variable will be multiplied by the calculated weight.
-        double questionWeight = findWeightByType(matchedKeywords.get("question"), answer.getQuestionsKeywords());
-
-        if(questionWeight != 0){
             double auxiliaryWeight = findWeightByType(matchedKeywords.get("auxiliary"), answer.getSecondaryKeywords());
             double topicWeight = findWeightByType(matchedKeywords.get("topic"), answer.getTertiaryKeywords());
 
             // The topic should weigh more to find the best answer for a question. The multiplication makes sure it has more impact than the auxiliary weight.
             resultedWeight = auxiliaryWeight + (topicWeight * 4);
-        }
-
         // The max weight is a 10:  1 * (2 + (4 * 2))
         // The lowest weight would be a 0.
         return resultedWeight;
@@ -133,22 +118,9 @@ public class InputProcessingServiceImpl implements InputProcessingService {
                 initial.add(keywords.get(i));
             }
         }
-
-        // Maybe adding it in the for loop might be better?
-        List<Long> results = initial.stream()
+        return initial.stream()
                 .map(Keyword::getId)
                 .collect(Collectors.toList());
-
-        return results;
-
-//        long[] results = new long[initial.size()];
-//        //im a genius lol
-//        int j = 0;
-//        for(Keyword k : initial){
-//          results[j++] = k.getId();
-//        }
-//
-//        return results;
     }
 
 
