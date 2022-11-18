9c48085e5b75370dd266b4cb33484248c744803a
Tsvetislav Rangelov <cveti2002mnt@gmail.com>
2022-11-18 23:52:50 +0100

    returning first 3 links on google search results

--- src/main/java/sem3chatbot/backend/business/ServiceImpl/SearchEngineServiceImpl.java
+++ src/main/java/sem3chatbot/backend/business/ServiceImpl/SearchEngineServiceImpl.java
@@ -2,6 +2,7 @@ package sem3chatbot.backend.business.ServiceImpl;
 
 import lombok.RequiredArgsConstructor;
 import org.jsoup.Jsoup;
+import org.jsoup.helper.Validate;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -19,24 +20,34 @@ public class SearchEngineServiceImpl implements SearchEngineService {
 
     @Override
     public SearchEngineTopThreeResponse getTopLinksFromSearchQuery(String queryString) throws IOException {
-        System.out.println("Searching...");
+        print("Searching...");
         String searchUrl = "https://google.com/search?q";
         Document rawHtml = Jsoup.connect(searchUrl + queryString)
                 .get();
         Set<String> links = findLinks(rawHtml);
-        System.out.println(links);
+        print("Links are :" + links);
         return SearchEngineTopThreeResponse.builder()
                 .links(links)
                 .build();
     }
 
     private Set<String> findLinks(Document html){
-        Set<String> results = new HashSet<>();;
-        Elements links = html.getElementsByTag("a");
+        Set<String> results = new HashSet<>();
+        //the selector here for the query needs to be correct to actually get search result links
+        Elements links = html.getElementsByAttribute("href");
+        if(links.size() == 0){
+            return new HashSet<String>();
+        }
+        print(links.text());
 
         for(int i = 0; i < 3; i++){
-            results.add(links.get(i).attr("abs:href"));
+            results.add(links.get(i).attr("href"));
         }
         return results;
+    }
+
+    //this is just for convenience in order to not call the sysout function over and over
+    private static void print(String text, Object... args){
+        System.out.printf((text) + "%n", args);
     }
 }
