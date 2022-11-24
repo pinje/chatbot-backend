8500427b0e91947b45fa6e423442ca3f46d062d9
Tsvetislav Rangelov <cveti2002mnt@gmail.com>
2022-11-24 13:53:37 +0100

    disabling login form with spring security

---
+++ src/main/java/sem3chatbot/backend/utils/WebSecurityConfig.java
@@ -0,0 +1,15 @@
+package sem3chatbot.backend.utils;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+public class WebSecurityConfig {
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .formLogin().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+         }
+    }
